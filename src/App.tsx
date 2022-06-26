e9e684c5cc0fdb3814a1f3858169e40fd9f1d951
ravielze <ravielz20@gmail.com>
2022-06-26 18:34:58 +0700

    Init dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import { Suspense } from 'react';
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import { Routing } from './routing';
+import 'react-toastify/dist/ReactToastify.css';
+import 'antd/dist/antd.less';
 
 function App() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <BrowserRouter>
-                <Routes>
-                    {Routing.map((route) => {
-                        const Component = route.component;
-                        return (
-                            <Route
-                                caseSensitive
-                                path={route.path}
-                                key={route.path}
-                                element={<Component />}
-                            />
-                        );
-                    })}
-                </Routes>
-            </BrowserRouter>
-        </Suspense>
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <BrowserRouter>
+                    <Routes>
+                        {Routing.map((route) => {
+                            const Component = route.component;
+                            return (
+                                <Route
+                                    caseSensitive
+                                    path={route.path}
+                                    key={route.path}
+                                    element={<Component />}
+                                />
+                            );
+                        })}
+                    </Routes>
+                </BrowserRouter>
+            </Suspense>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover={false}
+            />
+        </>
     );
 }
 
--- src/routing.ts
+++ src/routing.ts
@@ -1,26 +1,14 @@
 import React, { LazyExoticComponent } from 'react';
 
-const Page1 = React.lazy(() =>
-    import('./pages/Example').then((module) => ({ default: module.Page1 }))
-);
-
-const Page2 = React.lazy(() =>
-    import('./pages/Example').then((module) => ({ default: module.Page2 }))
-);
-
-const Page3 = React.lazy(() =>
-    import('./pages/Example').then((module) => ({ default: module.Page3 }))
-);
-
 const DefPage = React.lazy(() =>
-    import('./pages/Example').then((module) => ({
-        default: module.DefaultPage,
+    import('./pages/Dashboard').then((module) => ({
+        default: module.Dashboard,
     }))
 );
 
 const PageNotFound = React.lazy(() =>
-    import('./pages/Example').then((module) => ({
-        default: module.PageNotFound,
+    import('./pages/Dashboard').then((module) => ({
+        default: module.Dashboard,
     }))
 );
 
@@ -41,22 +29,6 @@ export const Routing: PageRouting[] = [
         title: 'Default Page',
         path: '/',
         component: DefPage,
-    },
-
-    {
-        title: 'Page 1',
-        path: '/page1',
-        component: Page1,
-    },
-    {
-        title: 'Page 2',
-        path: '/page2',
-        component: Page2,
-    },
-    {
-        title: 'Page 3',
-        path: '/page3',
-        component: Page3,
     },
     PageNotFoundRouting,
 ];
