582b4ad8ae04a1829916dbf35851f839a298de9a
ravielze <ravielz20@gmail.com>
2022-06-26 18:48:36 +0700

    Refactor & Add Standard Layout

---
+++ src/layout/StandardLayout.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { MenuProps, Breadcrumb, Layout, Menu } from 'antd';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import SkeletonAvatar from 'antd/lib/skeleton/Avatar';
+
+const { Header, Content, Footer, Sider } = Layout;
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[]
+): MenuItem {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+}
+
+const items: MenuItem[] = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [
+        getItem('Team 1', '6'),
+        getItem('Team 2', '8'),
+    ]),
+    getItem('Files', '9', <FileOutlined />),
+];
+export interface StandardLayoutProps {
+    children?: JSX.Element | JSX.Element[] | string | string[];
+}
+
+export const StandardLayout = ({
+    children = undefined,
+}: StandardLayoutProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(value) => setCollapsed(value)}
+                style={{ minHeight: '100vh' }}
+            >
+                <div className="flex justify-center my-2 md:my-3">
+                    <SkeletonAvatar size="large" />
+                </div>
+                <Menu
+                    theme="dark"
+                    defaultSelectedKeys={['1']}
+                    mode="inline"
+                    items={items}
+                />
+            </Sider>
+            <Layout>
+                <Header className="bg-gray-100" style={{ padding: 0 }}>
+                    <Breadcrumb style={{ margin: '16px 0' }} className="pl-5">
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                </Header>
+                <Content style={{ margin: '0 16px' }}>
+                    <div
+                        className="bg-white text-xl"
+                        style={{ padding: 24, minHeight: 360 }}
+                    >
+                        {children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
