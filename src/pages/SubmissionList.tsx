ea84c71ecf27e51ab314b8c9adc87db7ab86d756
Fabian <13519140@std.stei.itb.ac.id>
2022-06-28 15:20:48 +0700

    add action button

--- src/pages/SubmissionList.tsx
+++ src/pages/SubmissionList.tsx
@@ -1,9 +1,11 @@
-import { Table, PageHeader } from 'antd';
+import { Table, PageHeader, Space, Button } from 'antd';
+import { FolderOpenOutlined } from '@ant-design/icons';
 import type { ColumnsType } from 'antd/lib/table';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { StandardLayout } from '../layout/StandardLayout';
 
 interface DataType {
+    id: number;
     submission: string;
     nim: string;
 }
@@ -43,26 +45,48 @@ const columns: ColumnsType<DataType> = [
         onFilter: (value: any, record) => record.nim.indexOf(value) === 0,
         sorter: (a, b) => a.nim.localeCompare(b.nim),
     },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (_, record) => (
+            <Space size="middle">
+                <Link to={`${record.id}`}>
+                    <Button
+                        type="primary"
+                        icon={<FolderOpenOutlined />}
+                        size="large"
+                    >
+                        Open
+                    </Button>
+                </Link>
+            </Space>
+        ),
+    },
 ];
 
 const data = [
     {
+        id: 1,
         submission: 'John Brown',
         nim: '13519001',
     },
     {
+        id: 2,
         submission: 'John Brown',
         nim: '13519002',
     },
     {
+        id: 3,
         submission: 'John Brown',
         nim: '13519003',
     },
     {
+        id: 4,
         submission: 'John Brown',
         nim: '13519004',
     },
     {
+        id: 5,
         submission: 'John Brown',
         nim: '13519005',
     },
