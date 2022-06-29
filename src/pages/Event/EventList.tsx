f56ae825775c324ced2182697e0cfe43e4c1da08
airathalca <13520101@std.stei.itb.ac.id>
2022-06-29 19:31:23 +0700

    Update Event Listing Page

--- src/pages/Event/EventList.tsx
+++ src/pages/Event/EventList.tsx
@@ -1,120 +1,103 @@
 import { PlusOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { Table, Button } from 'antd';
-import { Link } from 'react-router-dom';
+import { Table, Button, PageHeader } from 'antd';
+import { ColumnsType } from 'antd/lib/table';
+import { Link, useNavigate } from 'react-router-dom';
 import { StandardLayout } from '../../layout/StandardLayout';
 
+interface EventData {
+    id: number;
+    eventName: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+    method: string;
+}
+
+const dataDummy = [
+    {
+        id: 1,
+        eventName: 'Event 1',
+        date: '2020-01-01',
+        startTime: '',
+        endTime: '',
+        method: '',
+    },
+    {
+        id: 2,
+        eventName: 'Event 2',
+        date: '2020-01-02',
+        startTime: '',
+        endTime: '',
+        method: '',
+    },
+    {
+        id: 3,
+        eventName: 'Event 3',
+        date: '2020-01-03',
+        startTime: '',
+        endTime: '',
+        method: '',
+    },
+    {
+        id: 4,
+        eventName: 'Event 4',
+        date: '2020-01-04',
+        startTime: '',
+        endTime: '',
+        method: '',
+    },
+    {
+        id: 5,
+        eventName: 'Event 5',
+        date: '2020-01-05',
+        startTime: '',
+        endTime: '',
+        method: '',
+    },
+    {
+        id: 6,
+        eventName: 'Event 6',
+        date: '2020-01-06',
+        startTime: '',
+        endTime: '',
+        method: '',
+    },
+];
+
+const columns: ColumnsType<EventData> = [
+    {
+        title: 'Name',
+        dataIndex: 'eventName',
+        width: 550,
+        filters: [],
+        onFilter: (value: any, record) => record.eventName.indexOf(value) === 0,
+        sorter: (a, b) => a.eventName.localeCompare(b.eventName),
+    },
+    {
+        title: 'Date',
+        dataIndex: 'date',
+        width: 550,
+        sorter: (a, b) => a.eventName.localeCompare(b.eventName),
+    },
+    {
+        title: 'Details',
+        render: (_, record) => (
+            <Link to={`/event/${record.eventName}`}>
+                <Button
+                    type="primary"
+                    size="large"
+                    icon={<InfoCircleOutlined className="align-baseline" />}
+                    className="bg-blue-500 hover:bg-blue-350"
+                >
+                    Details
+                </Button>
+            </Link>
+        ),
+    },
+];
+
 export const EventList = () => {
-    const dataDummy = [
-        {
-            id: 1,
-            eventName: 'sahfjkashfkjahskfa',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 2,
-            eventName: 'ajdghjkasgdmnabscx',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 3,
-            eventName: 'ajsdyaukedbakbjsdsa',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 4,
-            eventName: 'asdgasjkdasjkhdkas',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 5,
-            eventName: 'qweuqyaslkdhlakj',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 6,
-            eventName: 'msbdabmdasbduasgdajs',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 7,
-            eventName: 'zxciuozxucioahdams',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 8,
-            eventName: 'adjkash jkasdsadnal adsa',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 9,
-            eventName: 'jsdajkashjkdh asdhasklj',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-        {
-            id: 10,
-            eventName: 'adas fahsudhafskjdh',
-            date: '',
-            startTime: '',
-            endTime: '',
-            method: '',
-        },
-    ];
-
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'eventName',
-            width: 550,
-        },
-        {
-            title: 'Date',
-            dataIndex: 'date',
-            width: 550,
-        },
-        {
-            title: 'Details',
-            render: (_: any, record: { id: any }) => (
-                <Link to={`/event/${record.id}`}>
-                    <Button
-                        type="primary"
-                        size="large"
-                        icon={<InfoCircleOutlined className="align-baseline" />}
-                        className="bg-blue-500 hover:bg-blue-350"
-                    >
-                        Details
-                    </Button>
-                </Link>
-            ),
-        },
-    ];
-
+    const navigate = useNavigate();
     return (
         <StandardLayout>
             <div className="mb-5">
@@ -129,19 +112,8 @@ export const EventList = () => {
                     </Button>
                 </Link>
             </div>
-            <div>
-                <h1 className="font-bold mb-3">List Event</h1>
-                <Table
-                    columns={columns}
-                    dataSource={dataDummy}
-                    pagination={{
-                        pageSize: 5,
-                    }}
-                    scroll={{
-                        y: 1000,
-                    }}
-                />
-            </div>
+            <PageHeader onBack={() => navigate(-1)} title="List Event" />
+            <Table columns={columns} dataSource={dataDummy} />
         </StandardLayout>
     );
 };
