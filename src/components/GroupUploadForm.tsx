c6ec5b0c36b66ec16e95f8d42584c56f014ccc72
msyahrulsp <mssp892@gmail.com>
2022-06-30 18:48:21 +0700

    add excel file reader

--- src/components/GroupUploadForm.tsx
+++ src/components/GroupUploadForm.tsx
@@ -1,49 +1,58 @@
-import { Form, Upload, Button, Input } from 'antd';
+import { Form, Upload, Button } from 'antd';
 import { InboxOutlined } from '@ant-design/icons';
+import readXlsxFile from 'read-excel-file';
 
-export const GroupUploadForm = () => (
-    <Form layout="vertical">
-        <Form.Item
-            label="Nama Kelompok"
-            name="nama-kelompok"
-            rules={[
-                {
-                    required: true,
-                    message: 'Nama kelompok belum diisi',
-                },
-            ]}
-        >
-            <Input placeholder="Ex: Kelompok 1" />
-        </Form.Item>
-        <Form.Item
-            label="Upload File Kelompok"
-            name="file-kelompok-upload"
-            rules={[
-                {
-                    required: true,
-                    message: 'File kelompok belum di-upload',
-                },
-            ]}
-        >
-            <Upload.Dragger name="files" multiple={false}>
-                <p className="ant-upload-drag-icon">
-                    <InboxOutlined />
-                </p>
-                <p className="ant-upload-text">Upload File Kelompok</p>
-            </Upload.Dragger>
-        </Form.Item>
+export const GroupUploadForm = () => {
+    const handleChange = async (file: any) => {
+        const data = await readXlsxFile(file.originFileObj);
+        console.log(data.splice(1, data.length));
+    };
 
-        <div className="flex gap-3">
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
+    return (
+        <Form layout="vertical">
+            <Form.Item
+                label="Upload File Kelompok"
+                name="file-kelompok-upload"
+                rules={[
+                    {
+                        required: true,
+                        message: 'File kelompok belum di-upload',
+                    },
+                ]}
+            >
+                <Upload.Dragger
+                    name="files"
+                    multiple={false}
+                    onChange={(e) => handleChange(e.fileList[0])}
+                    showUploadList={{ showRemoveIcon: true }}
+                    accept=".xlsx,.xls,.csv"
+                >
+                    <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                    </p>
+                    <p className="ant-upload-text">Upload File Kelompok</p>
+                </Upload.Dragger>
             </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="reset">
-                    Reset
-                </Button>
-            </Form.Item>
-        </div>
-    </Form>
-);
+
+            <div className="flex gap-2 flex-wrap">
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        onClick={() => console.log('Submit')}
+                    >
+                        Submit
+                    </Button>
+                </Form.Item>
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        danger
+                        onClick={() => console.log('Delete All')}
+                    >
+                        Delete All
+                    </Button>
+                </Form.Item>
+            </div>
+        </Form>
+    );
+};
