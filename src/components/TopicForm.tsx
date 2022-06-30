9c09fed2653b471418721ce9b4621e350ce6ed3a
rayhankinan <rayhankinan@gmail.com>
2022-06-30 17:24:20 +0700

    Revert "add calendar"
    This reverts commit 3b799dd5479c3445bd7f61cb4c5db5c972205b45.

--- src/components/TopicForm.tsx
+++ src/components/TopicForm.tsx
@@ -5,57 +5,60 @@ interface Props {
     isCreating?: boolean;
 }
 
-export const TopicForm = ({ isCreating = true }: Props) => (
-    <Form layout="vertical" labelCol={{ span: 2 }} wrapperCol={{ span: 8 }}>
-        <Form.Item
-            label="Title"
-            name="title"
-            rules={[{ required: true, message: 'Judul topik belum diisi' }]}
-        >
-            <Input placeholder="Ex: Topik 1" />
-        </Form.Item>
-        <Form.Item
-            label="Deadline"
-            name="deadline"
-            rules={[
-                {
-                    required: true,
-                    message: 'Waktu pengerjaan topik belum diisi',
-                },
-            ]}
-        >
-            <DatePicker.RangePicker showTime />
-        </Form.Item>
-        {isCreating ? (
+export const TopicForm = ({ isCreating = true }: Props) => {
+    
+    return (
+        <Form layout="vertical" labelCol={{ span: 2 }} wrapperCol={{ span: 8 }}>
             <Form.Item
-                label="Upload File Soal"
-                name="file-upload"
+                label="Title"
+                name="title"
+                rules={[{ required: true, message: 'Judul topik belum diisi' }]}
+            >
+                <Input placeholder="Ex: Topik 1" />
+            </Form.Item>
+            <Form.Item
+                label="Deadline"
+                name="deadline"
                 rules={[
-                    { required: true, message: 'File soal belum di-upload!' },
+                    {
+                        required: true,
+                        message: 'Waktu pengerjaan topik belum diisi',
+                    },
                 ]}
             >
-                <Upload.Dragger name="file" multiple={false}>
-                    <p className="ant-upload-drag-icon">
-                        <InboxOutlined />
-                    </p>
-                    <p className="ant-upload-text">Upload File Soal</p>
-                </Upload.Dragger>
+                <DatePicker.RangePicker showTime />
             </Form.Item>
-        ) : (
-            <Form.Item
-                label="Dropdown (TBD)"
-                name="dropdown"
-                rules={[{ required: true, message: 'Dropdown' }]}
-            >
-                <Select>
-                    <Select.Option value="jack">Jack</Select.Option>
-                </Select>
+            {isCreating ? (
+                <Form.Item
+                    label="Upload File Soal"
+                    name="file-upload"
+                    rules={[
+                        { required: true, message: 'File soal belum di-upload!' },
+                    ]}
+                >
+                    <Upload.Dragger name="file" multiple={false}>
+                        <p className="ant-upload-drag-icon">
+                            <InboxOutlined />
+                        </p>
+                        <p className="ant-upload-text">Upload File Soal</p>
+                    </Upload.Dragger>
+                </Form.Item>
+            ) : (
+                <Form.Item
+                    label="Dropdown (TBD)"
+                    name="dropdown"
+                    rules={[{ required: true, message: 'Dropdown' }]}
+                >
+                    <Select>
+                        <Select.Option value="jack">Jack</Select.Option>
+                    </Select>
+                </Form.Item>
+            )}
+            <Form.Item>
+                <Button size="large" type="primary" htmlType="submit">
+                    Submit
+                </Button>
             </Form.Item>
-        )}
-        <Form.Item>
-            <Button size="large" type="primary" htmlType="submit">
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>
-);
+        </Form>
+    );
+}
