3a33ab7f6e851dc0b9ca54b1f2344b987682777f
jasonk19 <13520080@std.stei.itb.ac.id>
2022-07-02 09:07:44 +0700

    Create, Edit, Delete Event Integration

--- src/pages/event/Detail.tsx
+++ src/pages/event/Detail.tsx
@@ -1,44 +1,106 @@
+import { useState, useEffect } from 'react';
+import moment from 'moment';
 import { useParams, useNavigate } from 'react-router-dom';
-import { Button, Form, Input, PageHeader } from 'antd';
+import { Button, Form, Input, PageHeader, Spin } from 'antd';
+import eventService from '../../service/event';
+import { defaultFailureCallback } from '../../service';
 import { StandardLayout } from '../../layout/StandardLayout';
 
 export const EventDetail = () => {
     const { id } = useParams();
     const navigate = useNavigate();
 
+    const [title, setTitle] = useState('');
+    const [attendanceStart, setAttendanceStart] = useState('');
+    const [attendanceEnd, setAttendanceEnd] = useState('');
+    const [attendanceType, setAttendanceType] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = async () => {
+        await eventService.getEventById(
+            id,
+            (res) => {
+                setTitle(res.title);
+                setAttendanceStart(res.attendance_start);
+                setAttendanceEnd(res.attendance_end);
+                setAttendanceType(res.attendance_type);
+                setLoading(false);
+            },
+            (err) => {
+                defaultFailureCallback(err);
+                setLoading(false);
+            }
+        );
+    };
+
+    const handleDeleteEvent = async () => {
+        await eventService.deleteEvent(
+            id,
+            (res) => console.log({ message: 'Success', data: res }),
+            (err) => defaultFailureCallback(err)
+        );
+        navigate('/event');
+    };
+
+    const convertToDateTime = (isoString: string) => {
+        const date = moment(isoString);
+        const dateComponent = date.format('DD MMM YY');
+        const timeComponent = date.format('HH:mm:ss');
+
+        return `${dateComponent} ${timeComponent}`;
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <StandardLayout>
             <PageHeader onBack={() => navigate(-1)} title="Event Detail" />
-            <Form
-                layout="vertical"
-                labelCol={{ span: 5 }}
-                wrapperCol={{ span: 8 }}
-            >
-                <Form.Item label="Event Name" name="event-name">
-                    <Input defaultValue={id} disabled />
-                </Form.Item>
-                <Form.Item label="Attendance Start" name="attendance-start">
-                    <Input defaultValue={id} disabled />
-                </Form.Item>
-                <Form.Item label="Attendance End" name="attendance-end">
-                    <Input defaultValue={id} disabled />
-                </Form.Item>
-                <Form.Item label="Attendance Type" name="attendance-type">
-                    <Input defaultValue={id} disabled />
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        type="primary"
-                        style={{ marginRight: 10 }}
-                        onClick={() => navigate(`/event/${id}/edit`)}
-                    >
-                        Edit
-                    </Button>
-                    <Button type="primary" danger>
-                        Delete
-                    </Button>
-                </Form.Item>
-            </Form>
+            {loading ? (
+                <Spin tip="Loading..." />
+            ) : (
+                <Form
+                    layout="vertical"
+                    labelCol={{ span: 5 }}
+                    wrapperCol={{ span: 8 }}
+                    initialValues={{
+                        title,
+                        attendance_start: convertToDateTime(attendanceStart),
+                        attendance_end: convertToDateTime(attendanceEnd),
+                        attendance_type: attendanceType,
+                    }}
+                >
+                    <Form.Item label="Event Name" name="title">
+                        <Input disabled />
+                    </Form.Item>
+                    <Form.Item label="Attendance Start" name="attendance_start">
+                        <Input disabled />
+                    </Form.Item>
+                    <Form.Item label="Attendance End" name="attendance_end">
+                        <Input disabled />
+                    </Form.Item>
+                    <Form.Item label="Attendance Type" name="attendance_type">
+                        <Input disabled />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            style={{ marginRight: 10 }}
+                            onClick={() => navigate(`/event/${id}/edit`)}
+                        >
+                            Edit
+                        </Button>
+                        <Button
+                            type="primary"
+                            danger
+                            onClick={handleDeleteEvent}
+                        >
+                            Delete
+                        </Button>
+                    </Form.Item>
+                </Form>
+            )}
         </StandardLayout>
     );
 };
--- src/pages/event/Edit.tsx
+++ src/pages/event/Edit.tsx
@@ -1,82 +1,181 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { Button, Form, Input, DatePicker, Select } from 'antd';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import type { DatePickerProps } from 'antd';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Button, Form, Input, DatePicker, Select, Spin } from 'antd';
+import eventService from '../../service/event';
+import { defaultFailureCallback } from '../../service';
 import { StandardLayout } from '../../layout/StandardLayout';
 
 export const EditEvent = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState('');
+    const [attendanceStart, setAttendanceStart] = useState('');
+    const [attendanceEnd, setAttendanceEnd] = useState('');
+    const [attendanceType, setAttendanceType] = useState('');
+    const [empty, setEmpty] = useState(true);
+
+    const fetchData = async () => {
+        await eventService.getEventById(
+            id,
+            (res) => {
+                setTitle(res.title);
+                setAttendanceStart(res.attendance_start);
+                setAttendanceEnd(res.attendance_end);
+                setAttendanceType(res.attendance_type);
+                setEmpty(false);
+            },
+            (err) => {
+                defaultFailureCallback(err);
+                setEmpty(false);
+            }
+        );
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const handleEventNameChange = (e: any) => {
+        setTitle(e.target.value);
+    };
+
+    const handleAttendanceStartChange = (
+        _: DatePickerProps['value'],
+        dateString: string
+    ) => {
+        setAttendanceStart(dateString);
+    };
+
+    const handleAttendanceEndChange = (
+        _: DatePickerProps['value'],
+        dateString: string
+    ) => {
+        setAttendanceEnd(dateString);
+    };
+
+    const handleAttendanceTypeChange = (value: string) => {
+        setAttendanceType(value);
+    };
+
+    const handleSubmit = async () => {
+        const data = {
+            title,
+            attendance_start: new Date(attendanceStart).toISOString(),
+            attendance_end: new Date(attendanceEnd).toISOString(),
+            attendance_type: attendanceType,
+        };
+        await eventService.updateEvent(
+            id,
+            data,
+            (res) => console.log({ message: 'Success', data: res }),
+            (err) => defaultFailureCallback(err)
+        );
+        navigate(`/event/${id}`);
+    };
+
     return (
         <StandardLayout>
             <h1 className="font-bold mb-3">Edit Event</h1>
-            <Form
-                layout="vertical"
-                labelCol={{ span: 5 }}
-                wrapperCol={{ span: 8 }}
-            >
-                <Form.Item
-                    label="Event Name"
-                    name="event-name"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input the event name!',
-                        },
-                    ]}
+            {empty ? (
+                <Spin tip="Loading..." />
+            ) : (
+                <Form
+                    layout="vertical"
+                    labelCol={{ span: 5 }}
+                    wrapperCol={{ span: 8 }}
+                    initialValues={{
+                        title,
+                        attendance_start: moment(attendanceStart),
+                        attendance_end: moment(attendanceEnd),
+                        attendance_type: attendanceType,
+                    }}
                 >
-                    <Input name="eventName" defaultValue={id} />
-                </Form.Item>
-                <Form.Item
-                    label="Attendance Start"
-                    name="attendance-start"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input attendance start time!',
-                        },
-                    ]}
-                >
-                    <DatePicker showTime style={{ width: '100%' }} />
-                </Form.Item>
-                <Form.Item
-                    label="Attendance End"
-                    name="attendance-end"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input attendance end time!',
-                        },
-                    ]}
-                >
-                    <DatePicker showTime style={{ width: '100%' }} />
-                </Form.Item>
-                <Form.Item
-                    label="Attendance Type"
-                    name="attendance-type"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please pick attendance type!',
-                        },
-                    ]}
-                >
-                    <Select placeholder="Pick a method">
-                        <Select.Option value="group">
-                            Group Presence
-                        </Select.Option>
-                        <Select.Option value="self">
-                            Self Presence
-                        </Select.Option>
-                    </Select>
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" style={{ marginRight: 10 }}>
-                        Save
-                    </Button>
-                    <Button type="ghost">
-                        Cancel
-                    </Button>
-                </Form.Item>
-            </Form>
+                    <Form.Item
+                        label="Event Name"
+                        name="title"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input the event name!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            name="eventName"
+                            onChange={handleEventNameChange}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="Attendance Start"
+                        name="attendance_start"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input attendance start time!',
+                            },
+                        ]}
+                    >
+                        <DatePicker
+                            showTime
+                            style={{ width: '100%' }}
+                            onChange={handleAttendanceStartChange}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="Attendance End"
+                        name="attendance_end"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input attendance end time!',
+                            },
+                        ]}
+                    >
+                        <DatePicker
+                            showTime
+                            style={{ width: '100%' }}
+                            onChange={handleAttendanceEndChange}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        label="Attendance Type"
+                        name="attendance_type"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please pick attendance type!',
+                            },
+                        ]}
+                    >
+                        <Select
+                            placeholder="Pick a method"
+                            onChange={handleAttendanceTypeChange}
+                        >
+                            <Select.Option value="GroupLeader">
+                                Group Presence
+                            </Select.Option>
+                            <Select.Option value="Self">
+                                Self Presence
+                            </Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            onClick={handleSubmit}
+                            type="primary"
+                            style={{ marginRight: 10 }}
+                        >
+                            Save
+                        </Button>
+                        <Button type="ghost" onClick={() => navigate(-1)}>
+                            Cancel
+                        </Button>
+                    </Form.Item>
+                </Form>
+            )}
         </StandardLayout>
     );
 };
