43a5667958ae23d4cc9e58f4a58699bbfe0d4153
airathalca <13520101@std.stei.itb.ac.id>
2022-07-02 15:08:27 +0700

    list event integration update

--- src/pages/Event/EventList.tsx
+++ src/pages/Event/EventList.tsx
@@ -1,8 +1,8 @@
 import { PlusOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { Table, Button, Spin } from 'antd';
+import { Table, Button, Spin, PageHeader } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 import { useEffect, useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StandardLayout } from '../../layout/StandardLayout';
 import { defaultFailureCallback } from '../../service';
 import eventService, { IEvent } from '../../service/event';
@@ -49,30 +49,20 @@ const columns: ColumnsType<IEvent> = [
 ];
 
 export const EventList = () => {
+    const navigate = useNavigate();
     const [loading, setLoading] = useState<boolean>(false);
     const [events, setEvents] = useState<IEvent[]>([]);
-    const [page, setPage] = useState<number>(1);
-    const [pageSize, setPageSize] = useState<number>(10);
-    const [total, setTotal] = useState<number>(0);
-    const [queryParams, setQueryParams] = useSearchParams();
 
     useEffect(() => {
-        const queryPage = queryParams.get('page');
-        if (queryPage && +queryPage) {
-            setPage(+queryPage);
-        }
         setLoading(true);
-        eventService.getEvents(page, (data) => {
+        eventService.getEvents((data) => {
             setEvents(data.events);
-            setTotal(data.total);
-            setPage(data.page);
-            setPageSize(data.pageSize);
             setLoading(false);
         }, (err) => {
             defaultFailureCallback(err);
             setLoading(false);
         });
-    }, [page]);
+    });
 
     return (
         <StandardLayout>
@@ -87,14 +77,10 @@ export const EventList = () => {
                     </Button>
                 </Link>
             </div>
+            
             <Spin tip="Fetching data..." spinning={loading}>
-                <Table columns={columns} dataSource={events} pagination={{
-                    total, current: page, pageSize, showSizeChanger: false, onChange: (e) => {
-                        queryParams.set('page', e.toString());
-                        setQueryParams(queryParams);
-                        setPage(e);
-                    }
-                }} />
+                <PageHeader onBack={() => navigate(-1)} title="List Event" />
+                <Table columns={columns} dataSource={events} />
             </Spin>
         </StandardLayout>
     );
--- src/service/event.ts
+++ src/service/event.ts
@@ -9,7 +9,6 @@ import { APIErrorObject } from '../utils/api-error-object';
 
 export interface IEvent {
     id: string;
-    idx: number;
     title: string;
     attendance_start: string;
     attendance_end: string;
@@ -25,9 +24,6 @@ export interface REvent {
 
 export interface ListEvent {
     events: IEvent[];
-    page: number;
-    pageCount: number;
-    pageSize: number;
     total: number;
 }
 
@@ -80,41 +76,31 @@ class EventService extends GenericService {
     }
 
     public async getEvents(
-        pageNumber: number = 1,
         onSuccess?: SuccessCallbackFunction,
         onFail?: FailureCallbackFunction
         
     ) {
         const response = await APIClient.GET(`/events`, {
-            'pagination[pageSize]': 10,
-            'pagination[page]': pageNumber,
         });
         if (response instanceof APIErrorObject) {
             if (!onFail) return;
-
             return onFail(response);
         }
-
         const rawEvents: any[] = response.data;
-        const { page, pageCount, pageSize, total }: { [key: string]: number } =
-            response.meta.pagination;
-        const events: IEvent[] = rawEvents.map((eachRaw, eachIdx) => {
+        const events: IEvent[] = rawEvents.map((eachRaw, _) => {
             const each: any = eachRaw.attributes;
             return {
-                id: each.id,
+                id: each.ext_id,
                 title: each.title,
                 attendance_start: moment(each.attendance_start).format('DD MMM YY HH:mm:ss'),
                 attendance_end: moment(each.attendance_end).format('DD MMM YY HH:mm:ss'),
                 attendance_type: each.attendance_type,
-                idx: eachIdx + 1 + (page - 1) * pageSize,
             };
         });
+        const total = events.length;
         const mappedResponse: ListEvent = {
             events,
-            page,
-            pageCount,
-            total,
-            pageSize,
+            total
         };
         if (onSuccess) onSuccess(mappedResponse);
     }
