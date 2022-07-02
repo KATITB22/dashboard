044ff193c5c2fccf718a72d558fe4707b7a6c24e
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-07-02 19:59:44 +0700

    integrate with backend

--- src/pages/Group/GroupDetail.tsx
+++ src/pages/Group/GroupDetail.tsx
@@ -1,21 +1,29 @@
-import { PageHeader, Table } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import { PageHeader, Spin, Table } from 'antd';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { StandardLayout } from '../../layout/StandardLayout';
 
+import service, { GroupMember } from '../../service/group';
+
 export const GroupDetail = () => {
+    const { id } = useParams();
     const navigate = useNavigate();
-    const dataSource = [
-        {
-            key: '1',
-            name: 'Aira',
-            faculty: 'STEI',
-        },
-        {
-            key: '2',
-            name: 'Kinan',
-            faculty: 'STEI',
-        },
-    ];
+    const [members, setMembers] = useState<GroupMember[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    // const dataSource = [
+    //     {
+    //         key: '1',
+    //         name: 'Aira',
+    //         faculty: 'STEI',
+    //         campus: 'Ganesha',
+    //     },
+    //     {
+    //         key: '2',
+    //         name: 'Kinan',
+    //         faculty: 'STEI',
+    //         campus: 'Ganesha',
+    //     },
+    // ];
 
     const columns = [
         {
@@ -23,17 +31,32 @@ export const GroupDetail = () => {
             dataIndex: 'name',
             key: 'name',
         },
-        {
-            title: 'Faculty',
-            dataIndex: 'faculty',
-            key: 'faculty',
-        },
+        // {
+        //     title: 'Faculty',
+        //     dataIndex: 'faculty',
+        //     key: 'faculty',
+        // },
+        // {
+        //     title: 'Campus',
+        //     dataIndex: 'campus',
+        //     key: 'campus',
+        // },
     ];
+
+    useEffect(() => {
+        service.getGroupByID(id, (res) => {
+            setMembers(res.members);
+            setIsLoading(false);
+        });
+    }, []);
+
     return (
         <StandardLayout>
             <>
                 <PageHeader onBack={() => navigate(-1)} title="Group Detail" />
-                <Table dataSource={dataSource} columns={columns} />
+                <Spin tip="Fetching data..." spinning={isLoading}>
+                    <Table dataSource={members} columns={columns} />
+                </Spin>
             </>
         </StandardLayout>
     );
---
+++ src/service/group.ts
@@ -0,0 +1,84 @@
+import {
+    FailureCallbackFunction,
+    GenericService,
+    SuccessCallbackFunction,
+} from '.';
+import APIClient from '../utils/api-client';
+
+export interface Group {
+    id: string;
+    name: string;
+}
+
+export interface GroupResponse {
+    groups: Group[];
+    page: number;
+    pageCount: number;
+    pageSize: number;
+    total: number;
+}
+
+export interface GroupMember {
+    id: string;
+    name: string;
+    // faculty: string;
+}
+
+export interface GroupMemberResponse {
+    members: GroupMember[];
+}
+
+class GroupService extends GenericService {
+    public async getGroupByID(
+        id?: string,
+        onSuccess?: SuccessCallbackFunction,
+        onFail?: FailureCallbackFunction
+    ) {
+        const response = await APIClient.GET(`/groups/${id}`);
+        const members: GroupMember[] = response.members.map(
+            (each: any, eachIdx: number) => ({
+                id: eachIdx,
+                name: each,
+            })
+        );
+
+        const mappedResponse: GroupMemberResponse = {
+            members,
+        };
+        this.handleResponse(mappedResponse, onSuccess, onFail);
+    }
+
+    public async getGroups(
+        pageNumber: number = 1,
+        onSuccess?: SuccessCallbackFunction,
+        onFail?: FailureCallbackFunction
+    ) {
+        const response = await APIClient.GET('/groups', {
+            'pagination[pageSize]': 10,
+            'pagination[page]': pageNumber,
+        });
+
+        const { page, pageCount, pageSize, total }: { [key: string]: number } =
+            response.meta.pagination;
+        const raw: any[] = response.data;
+        const groups: Group[] = raw.map((eachRaw: any) => {
+            const each: any = eachRaw.attributes;
+            return {
+                id: each.id,
+                name: each.name,
+            };
+        });
+        const mappedResponse: GroupResponse = {
+            groups,
+            page,
+            pageCount,
+            total,
+            pageSize,
+        };
+
+        this.handleResponse(mappedResponse, onSuccess, onFail);
+    }
+}
+
+const service = new GroupService();
+export default service;
