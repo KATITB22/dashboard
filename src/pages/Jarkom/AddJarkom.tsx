274d0885106a014c9e647807e7122763e6dcd76a
msyahrulsp <mssp892@gmail.com>
2022-07-24 20:36:29 +0700

    Add jarkom page

---
+++ src/pages/Jarkom/AddJarkom.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { PageHeader, Form, Input, Button, Modal } from "antd";
+import { useNavigate } from "react-router-dom"
+import { StandardLayout } from "../../layout/StandardLayout";
+import ReactMarkdown from "react-markdown";
+
+export const AddJarkom = () => {
+    const [data, setData] = useState({
+        title: '',
+        jarkom: '',
+    });
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+    const navigate = useNavigate();
+    const { TextArea } = Input; 
+
+    const handleJarkomTitleChange = (e: any) => {
+        setData({
+            ...data,
+            title: e.target.value,
+        });
+    }
+
+    const handleJarkomChange = (e: any) => {
+        setData({
+            ...data,
+            jarkom: e.target.value,
+        });
+    }
+
+    return (
+        <StandardLayout allowedRole={"Committee"}>
+            <PageHeader onBack={() => navigate(-1)} title="Add Jarkom" />
+            <Form
+                layout="vertical"
+                wrapperCol={{ span: 10 }}
+            >
+                <Form.Item
+                    label="Jarkom Title"
+                    name="jarkom-title"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your jarkom title!',
+                        },
+                    ]}
+                >
+                    <Input
+                        placeholder="Input your jarkom title"
+                        onChange={handleJarkomTitleChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    label="Jarkom"
+                    name="jarkom"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your jarkom!',
+                        },
+                    ]}
+                >
+                    <TextArea
+                        rows={4}
+                        placeholder="Input your jarkom"
+                        allowClear
+                        onChange={handleJarkomChange}
+                    />
+                </Form.Item>
+                <div className="flex flex-row gap-2 flex-wrap">
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            onClick={() => setIsVisible(true)}
+                        >
+                            Preview
+                        </Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            htmlType="reset"
+                            onClick={() => console.log("Pie kabare")}
+                        >
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </div>
+            </Form>
+            <Modal
+                title="Preview Jarkom"
+                visible={isVisible}
+                onOk={() => setIsVisible(false)}
+                onCancel={() => setIsVisible(false)}
+            >
+                {data.jarkom === "" ? "Empty" :
+                    <ReactMarkdown>
+                        {data.jarkom}
+                    </ReactMarkdown>
+                }
+            </Modal>
+        </StandardLayout>
+    )
+}
---
+++ src/pages/Jarkom/Jarkom.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { List, Button, PageHeader, Avatar, Popover } from 'antd';
+import { StandardLayout } from "../../layout/StandardLayout";
+import ReactMarkdown from 'react-markdown';
+
+interface JarkomProps {
+    creator: string,
+    title: string,
+    text: string,
+} 
+
+export const Jarkom = () => {
+    const [data, setData] = useState<JarkomProps[]>([
+        {
+            creator: "PIE KABARE",
+            title: 'PIE',
+            text: "***ITALICBOLD***",
+        },
+        {
+            creator: "PIE KABARE",
+            title: 'NICE',
+            text: "# Title"
+        }
+    ]);
+    const navigate = useNavigate();
+    return (
+        <StandardLayout allowedRole={"Committee"}>
+            <PageHeader title="KAT ITB 2022 Jarkom" />
+            <div className='mb-3'>
+                <Button type="primary"
+                    onClick={() => navigate("/jarkom")}
+                >
+                    + Jarkom
+                </Button>
+            </div>
+            <List
+                itemLayout="horizontal"
+                size="large"
+                dataSource={data}
+                renderItem={item => (
+                    <List.Item>
+                        <List.Item.Meta
+                            avatar={
+                                <Popover content={item.creator}>
+                                    <Avatar>{item.creator[0]}</Avatar>
+                                </Popover>
+                            }
+                            title={item.title}
+                            description={<ReactMarkdown>{item.text}</ReactMarkdown>}
+                        />
+                    </List.Item>
+                )}
+            >
+            </List>
+        </StandardLayout>
+    )
+}
