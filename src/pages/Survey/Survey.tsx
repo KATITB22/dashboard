e3c9133ee98952cb69ef614effa452c1b6a4b28d
Fabian <13519140@std.stei.itb.ac.id>
2022-08-11 22:19:12 +0700

    submit form to backend

--- src/pages/Survey/Survey.tsx
+++ src/pages/Survey/Survey.tsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
-import { Alert, Button, PageHeader, Steps } from "antd";
+import { useState } from "react";
+import { Alert, Button, PageHeader, Spin, Steps } from "antd";
 import { StandardLayout } from "../../layout/StandardLayout";
 import { Event, Article, Assignment, Friend } from "../../components/Survey";
+import service from "../../service/survey";
+import { toast } from "react-toastify";
 
 interface SurveyData {
   event: Record<string, any>
@@ -12,6 +14,7 @@ interface SurveyData {
 
 export const Survey = () => {
   const [current, setCurrent] = useState(0);
+  const [loading, setLoading] = useState(true);
   const [event, setEvent] = useState<Record<string, any>>({})
   const [article, setArticle] = useState<Record<string, any>>({})
   const [assignment, setAssignment] = useState<Record<string, any>>({})
@@ -46,13 +49,17 @@ export const Survey = () => {
     setCurrent(current - 1);
   };
 
-  const handleSubmit = () => {
-    console.log(event, assignment, friend, article)
-  }
+  const handleSubmit = async () => {
+    setLoading(true)
+    const surveyData : SurveyData = {event, article, assignment, friend}
 
-  useEffect(() => {
-    console.log(friend)
-  }, [friend])
+    await service.postSurvey(surveyData, () => {
+      setLoading(false)
+      toast.success("Berhasil melakukan submit!");
+    }, (err) => {
+      toast.error("Gagal melakukan submit! " + err.toString())
+    })
+  }
 
   return (
     <StandardLayout allowedRole={["Committee", "Mentor", "Participant"]}>
@@ -62,37 +69,43 @@ export const Survey = () => {
           className='my-3'
           message="Announcement"
           description={
-              <p>Mohon isi form survei berikut sebagai bentuk feedback agar acara OSKM dapat menjadi lebih baik</p>
+            <p>Mohon isi form survei berikut sebagai bentuk feedback agar acara OSKM dapat menjadi lebih baik</p>
           }
           type="info"
-        />
+          />
       </div>
 
-      <div className="mt-10">
-        <Steps current={current}>
-          {steps.map(item => (
-              <Steps.Step key={item.title} title={item.title} />
-          ))}
-        </Steps>
-        {SurveyForm}
-        <div>
-          {current > 0 && (
-            <Button style={{ margin: '0 8px' }} onClick={prevPage}>
-                Previous
-            </Button>
-          )}
-          {current < steps.length - 1 && (
-            <Button type="primary" onClick={nextPage}>
-                Next
-            </Button>
-          )}
-          {current === steps.length - 1 && (
-            <Button type="primary" onClick={handleSubmit}>
-                Submit
-            </Button>
-          )}
+      {loading ? 
+        <div className="flex justify-center items-center">
+          <Spin tip="Submitting..." spinning={loading}></Spin>
         </div>
-      </div>
+        :
+        <div className="mt-10">
+          <Steps current={current}>
+            {steps.map(item => (
+              <Steps.Step key={item.title} title={item.title} />
+              ))}
+          </Steps>
+          {SurveyForm}
+          <div>
+            {current > 0 && (
+              <Button style={{ margin: '0 8px' }} onClick={prevPage}>
+                  Previous
+              </Button>
+            )}
+            {current < steps.length - 1 && (
+              <Button type="primary" onClick={nextPage}>
+                  Next
+              </Button>
+            )}
+            {current === steps.length - 1 && (
+              <Button type="primary" onClick={handleSubmit}>
+                  Submit
+              </Button>
+            )}
+          </div>
+        </div>
+      }
     </StandardLayout>
   )
 }
