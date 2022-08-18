1465798f924ea710a857c06c07e7cde452fdac95
Fabian <13519140@std.stei.itb.ac.id>
2022-08-18 14:24:10 +0700

    only menarik

--- src/components/Survey/Assignment.tsx
+++ src/components/Survey/Assignment.tsx
@@ -21,24 +21,6 @@ export const Assignment = ({data, setData}: Props) => {
           <Radio value={3}>Option C</Radio>
         </Space>
       </Radio.Group>
-
-      <h1>Menurutmu, tugas day berapa yang paling mudah?</h1>
-      <Radio.Group className="my-5" onChange={(e) => onChange(e, "mudah")} value={data["mudah"]}>
-        <Space direction="vertical">
-          <Radio value={1}>Option A</Radio>
-          <Radio value={2}>Option B</Radio>
-          <Radio value={3}>Option C</Radio>
-        </Space>
-      </Radio.Group>
-
-      <h1>Menurutmu, tugas day berapa yang paling susah?</h1>
-      <Radio.Group className="my-5" onChange={(e) => onChange(e, "susah")} value={data["susah"]}>
-        <Space direction="vertical">
-          <Radio value={1}>Option A</Radio>
-          <Radio value={2}>Option B</Radio>
-          <Radio value={3}>Option C</Radio>
-        </Space>
-      </Radio.Group>
     </div>
   )
 }
--- src/components/Survey/Event.tsx
+++ src/components/Survey/Event.tsx
@@ -20,15 +20,6 @@ export const Event = ({data, setData}: Props) => {
           <Radio value={"C"}>Option C</Radio>
         </Space>
       </Radio.Group>
-
-      <h1>Menurutmu, acara OSKM apa yang paling membosankan?</h1>
-      <Radio.Group className="my-5" onChange={(e) => onChange(e, "bosan")} value={data["bosan"]}>
-        <Space direction="vertical">
-          <Radio value={"A"}>Option A</Radio>
-          <Radio value={"B"}>Option B</Radio>
-          <Radio value={"C"}>Option C</Radio>
-        </Space>
-      </Radio.Group>
     </div>
   )
 }
